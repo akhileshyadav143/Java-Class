d2bf5db7b9a477f447fe3151bb4757cd1e62f9fe
akhileshyadav143 <akhileshyadav667543@gmail.com>
2025-11-19 16:01:14 +0530

    Java

--- overriding.java
+++ overriding.java
@@ -27,3 +27,21 @@
 //          s2.data(); 
 //         }
 //     }
+
+
+
+// Abstractions 
+abstract class Animal{
+    abstract void eat();
+}
+class cat extends Animal{
+    void eat(){
+        System.out.println("kitkat");
+    }
+}
+class main{
+    public static void main(String[] args){
+        cat s1=new cat();
+        s1.eat();
+    }
+}

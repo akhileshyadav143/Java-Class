cd7e16c3e0774eb05f90c6dbfc60a72f78bda129
akhileshyadav143 <akhileshyadav667543@gmail.com>
2025-11-20 16:10:16 +0530

    java

---
+++ Main.java
@@ -0,0 +1,68 @@
+// interface A {
+//     abstract void main();
+    
+// }
+// class B implements A{
+//     public void login(){
+//         System.out.println("hello");
+//     }
+// }
+// class hello{
+//     public static void main(String[] args){
+//         B s1=new B();
+//         s1.login();
+//     }
+// }
+
+
+
+
+// interface A {
+//     void walk();
+// }
+
+// interface B {
+//     void run();
+// }
+
+// class c implements A, B {
+
+//     public void walk() {
+//         System.out.println(" walking");
+//     }
+
+//     public void run() {
+//         System.out.println("running  ");
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         c h = new c();
+//         h.walk();
+//         h.run();
+//     }
+// }
+
+
+// interface A {
+//     void display();
+// }
+
+// interface B {
+//     void display();
+// }
+
+// class Demo implements A, B {
+
+//     public void display() {
+//         System.out.println("hiiiii...");
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Demo d = new Demo();
+//         d.display();
+//     }
+// }

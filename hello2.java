2e74a3912b7315d752bcf53e846b554a173d4110
akhileshyadav143 <akhileshyadav667543@gmail.com>
2025-11-21 21:16:51 +0530

    java

---
+++ hello2.java
@@ -0,0 +1,75 @@
+// class Atm{
+//     private int TotalBalance=10000;
+//     public void getB(){
+//      System.out.println(TotalBalance);
+//     }
+//     public void setBalance(int amount){
+//         if(amount>0){
+//            TotalBalance+=amount; 
+//         }
+//     }
+// }
+// class hello{
+//     public static void main(String[] args){
+//        Atm s1=new Atm();
+// s1.getB();
+// s1.setBalance(5000);
+//             }
+// }
+
+
+
+// class Youtube {
+//     private String channelname;
+//     private int subscribercount;
+
+//     public Youtube(String channelname) {
+//         this.channelname = channelname;
+//         this.subscribercount = 0;
+//     }
+
+
+//     public void getname(int pin) {
+//         if (pin == 1234) {
+//             System.out.println(channelname);
+//         }
+//     }
+
+
+//     public void getsubscribercount() {
+//         System.out.println("Subscribers = " + subscribercount);
+//     }
+
+//     public void setsubscribercount() {
+//         subscribercount++;   
+//     }
+//     public void setunsubscribercount() {
+//         if (subscribercount > 0) {
+//             subscribercount--;  
+//         } else {
+//             System.out.println("yeh nahi hai ");
+//         }
+//     }
+// }
+
+
+// class Hello {
+//     public static void main(String[] args) {
+//         Youtube s1 = new Youtube("Akhilesh@134");
+
+//         s1.getname(1234); 
+        
+//         s1.setsubscribercount();
+//         s1.setsubscribercount();
+//         s1.setsubscribercount();
+
+//         s1.getsubscribercount(); 
+
+       
+//         s1.setunsubscribercount(); 
+//         s1.setunsubscribercount(); 
+//         s1.setunsubscribercount(); 
+//         s1.setunsubscribercount(); 
+//     }
+// }
+

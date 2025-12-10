cf9ec9a02458953c67a7b8c788611291f20bd1b4
akhileshyadav143 <akhileshyadav667543@gmail.com>
2025-12-10 10:30:38 +0530

    Java

---
+++ java17.java
@@ -0,0 +1,48 @@
+
+
+// public class java17 {
+//     public static void main(String[] args){
+//      System.out.println("hello");
+//      try{
+//        // int a=5;
+//        // int b=0;
+//        // int c=a/b;
+//        String str=null;
+//         System.out.println(str);
+
+//      }catch(Exception e){
+//         System.out.println(e);
+//      }finally{
+//         System.out.println("hammesha chalaegeee..... "); 
+//      }
+//     // int a=5;
+//     // int b=5;
+//    // int b=0;
+//    //  int c=a/b;
+//    //  System.out.println(c);
+//      System.out.println("byiii");
+//     }
+// }
+
+// public class java17{
+//    public static void main(String[] args){
+//       try{
+//     static  voteAge(15);
+
+//    } catch (Exception e){
+//       System.out.println(e);
+//    } 
+//    try{
+//       voteAge(19);
+//    } catch (Exception e){
+//    }
+//    }
+//   static void voteaAge(int age){
+//     System.out.println("check for vote");
+//     if(age<18){
+//       throw new IllegalArgumentException("app vote nahi ker kakty hai"); 
+//     }
+//    }
+// }
+ 
+

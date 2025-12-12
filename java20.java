6eb0b67540df9a4750275f64f0146a99d03763cb
akhileshyadav143 <akhileshyadav667543@gmail.com>
2025-12-12 23:36:30 +0530

    Java

---
+++ java20.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+// public class java20 {
+//     public static void  main(String[] args){
+//       ArrayList<Integer>list=new ArrayList<>();
+//       list.add(5);
+//       list.add(6);
+//       list.add(7);
+//       list.add(8);
+//       List<Integer> res=  list.stream().filter(n->n%2!=0).toList();
+//       List<Integer> mapData=  list.stream().map(n->n*2).toList();
+//       int data= list.stream().reduce(0,(a,b)->a+b);
+//       List<Integer> skip=  list.stream().skip(2).toList();
+//       List<Integer> limit=  list.stream().limit(2).toList();
+//       List<Integer> sort=  list.stream().sorted((a,b)->a-b).toList();
+//       System.out.println(res);
+//       System.out.println(mapData);
+//       System.out.println(data);
+//       System.out.println(skip);
+//       System.out.println(limit);
+//       System.out.println(sort);
+//     }
+// }
+
+

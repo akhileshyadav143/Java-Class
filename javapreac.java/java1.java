ee007a31682438b442bbb93a3e4354fe10554dee
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-01-26 20:30:05 +0530

    java

---
+++ javapreac.java/java1.java
@@ -0,0 +1,16 @@
+
+
+// public class java1 {
+//     public static void main(String[] args) {
+//         System.out.println("hello would");
+//     }
+// }
+
+
+public class java1{
+    public static void main(String[] args) {
+        System.out.println("hello java with java");
+        System.out.println("hello java with java");
+        System.out.println("hello java with java");
+    }
+}

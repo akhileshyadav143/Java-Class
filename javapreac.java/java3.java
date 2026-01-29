5b570bddd0b65c4cfa322656a483cfdfdc1245a2
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-01-29 20:05:58 +0530

    java

---
+++ javapreac.java/java3.java
@@ -0,0 +1,50 @@
+// import java.util.Scanner;
+
+// public class java3 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int age =sc.nextInt();
+       
+//         if(age>18){
+//             System.out.println("adult");
+//          } else{
+//             System.out.println("not adult");
+//          }
+//         }
+//     }
+
+// import java.util.Scanner;
+// public class java3{
+//     public static void main(String args[]){
+
+    
+//     Scanner sc = new Scanner(System.in);
+//     int x= sc.nextInt();
+
+//     if(x%2==0){
+//         System.out.println("even");
+//     }else{
+//         System.out.println("odd");
+//     }
+// }
+// }
+
+
+
+import java.util.Scanner;
+public class java3{
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+    int b= sc.nextInt();
+    if(a==b){
+     System.out.println("is equal");
+    }else{
+        if(a>b){
+        System.out.println("is grater");
+    }else{
+        System.out.println("is lesser");
+    }
+    }
+}
+}

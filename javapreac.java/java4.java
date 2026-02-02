a087a16bc04400c4e08623e2a5dd34a6374c6a6a
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-02-02 18:02:04 +0530

    java

---
+++ javapreac.java/java4.java
@@ -0,0 +1,43 @@
+// public class java4 {
+//     public static void main(String[] args) {
+//         for(int counter =0;counter <3 ; counter=counter+1){
+//             System.out.println("Hello World");
+//         for( int i=0;i<11;i++){
+//            System.out.println(i);
+//         }
+//         }
+//     }
+
+//     int i=0;
+//     while (i<11){
+//         System.out.println(i);
+//         i=i+1;
+//     }
+// }
+// }
+
+
+// int i=0;
+// do{
+//     System.out.println(i);
+//     i++;  
+// }while(i<11);
+// }
+// }
+
+
+
+import java.util.*;
+public class java4 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+          int n= sc.nextInt();
+          int sum=0;
+          for( int i=0; i<=n; i++){
+            sum=sum+i;
+          }
+          System.out.println(sum);
+        }
+    }
+

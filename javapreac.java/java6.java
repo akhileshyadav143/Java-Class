1bff5b6c5989f705cdd8dee386fe5c7deb692d86
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-03-14 14:00:36 +0530

    java

--- javapreac.java/java6.java
+++ javapreac.java/java6.java
@@ -70,20 +70,20 @@
 // }
 
 
-public class java6{
-public static void main(String[] args){
-    int n=5;
+// public class java6{
+// public static void main(String[] args){
+//     int n=5;
 
-    for(int i=1; i<=n; i++){
-        for(int j=1;j<=i;j++){
-            int sum=i+j;
-            if(sum%2 ==0){
-                System.out.print("1");
-            }else{
-                System.out.print("0");
-            }
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=i;j++){
+//             int sum=i+j;
+//             if(sum%2 ==0){
+//                 System.out.print("1");
+//             }else{
+//                 System.out.print("0");
+//             }
 
-        }
-    }
-}
-}
+//         }
+//     }
+// }
+// }

4757a47c865b8a5d513548fcaf9f1597899ff7c0
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-03-15 17:54:44 +0530

    java

---
+++ javapreac.java/java9.java
@@ -0,0 +1,14 @@
+//functions
+import java.util.*;
+public class java9  {
+       public static void printMyName(String name) {
+        System.out.println(name);
+        return ;
+       }
+    public static void main(String[] args){
+       Scanner sc=new Scanner(System.in);
+       String name=sc.next();
+       printMyName(name); ///class function
+    }
+}Akil                 
+

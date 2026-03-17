031320ae2f4172244da8f4a983974ecb5ab24003
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-03-17 17:47:02 +0530

    java

--- javapreac.java/java9.java
+++ javapreac.java/java9.java
@@ -1,14 +1,59 @@
 //functions
 import java.util.*;
+// public class java9  {
+//        public static void printMyName(String name) {
+//         System.out.println(name);
+//         return ;
+//        }
+//     public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        String name=sc.next();
+//        printMyName(name); ///class function
+//     }
+// }  
+
+
+// public class java9  {
+//        public static int calculateSum (int a, int b) {
+//         int sum=a+b;
+//         return sum;
+//        }
+//     public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+
+//        int sum=calculateSum (a,b);
+//        System.out.println("sum of 2 number is:"+sum);
+//     }
+// }  
+
+// public class java9  {
+//        public static int calculateproduct (int a, int b) {
+//         int product=a*b;
+//         return product;
+//        }
+//     public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+
+//        int product=calculateproduct (a,b);
+//        System.out.println("product of 2 number is:"+product);
+//     }
+// }  
+
 public class java9  {
-       public static void printMyName(String name) {
-        System.out.println(name);
-        return ;
+       public static void  printfactorial( int n){
+         int factorial =1;
+         for(int i=n;i>=1;i--){
+          factorial= factorial*i;
+         }
+         
        }
+       
     public static void main(String[] args){
        Scanner sc=new Scanner(System.in);
-       String name=sc.next();
-       printMyName(name); ///class function
+        int n=sc.nextInt();
     }
-}Akil                 
-
+}  

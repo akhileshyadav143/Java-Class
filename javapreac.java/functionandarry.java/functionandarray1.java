825ca59fd314b23fa5dcb712708716536f8a192e
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-01 23:17:11 +0530

    java

---
+++ javapreac.java/functionandarry.java/functionandarray1.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+// public class functionandarray1{
+//     public static int add(int a,int b)
+//     {
+//         return a+b;
+//     }
+// public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//          int sum =  add(a,b);
+//          System.out.println(sum);
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class  functionandarray1{
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter number:");
+//         int num = sc.nextInt();
+
+//         System.out.println("Number = " + num);
+
+//         sc.close();
+//     }
+// }                                             
+
+
+
+
+//  public class functionandarray1{
+    
+ 
+//     public static int  frequncycount(int n,int d)
+//     {
+//         int count=0;
+//         while(n>0){
+//           int  rem=n%10;
+//           if(rem==d)
+//           count++;
+//       n=n/10;
+//         }
+//         return count;
+//     }
+// public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int d=sc.nextInt();
+//          int count =  frequncycount(n,d);
+//          System.out.println(count);
+
+//     }
+// }
+
+
+// wap to conver binnary.
+// public class functionandarray1{
+    
+ 
+//     public static int  convertion(int n,int d)
+//     {
+//                int binary=10;
+//         String  octal ="";
+//         while(n>0){
+//           int  rem=n%2;
+//           binary=rem+binary;
+           
+//       n=n/2;
+//         }
+//         return binary;
+//     }
+// public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int d=sc.nextInt();
+//          int binary =  convertion(n,d);
+//          System.out.println(binary);
+
+//     }
+// }
+

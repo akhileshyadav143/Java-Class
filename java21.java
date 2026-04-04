1fbd072107a2b7a4e579b33eaa360fe9efa5fa10
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-04 23:54:56 +0530

    java

---
+++ java21.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+// public class java21{
+//     public static void main(String args[]){
+//         Scanner sc=  new Scanner(System.in);
+//         String name=sc.nextLine();
+//         System.out.println("your name is:"+name);
+
+// //         String name="Tony";
+// //         String  fullName="Tony Stark";
+// //         String sentance="My name is Tony Stark";
+//     }
+//  }
+
+
+// public class java21{
+//     public static void main(String args[]){
+//      String firstName ="tony";
+//      String  lastName ="stack";
+//      String fullName= firstName + "@" + lastName;
+//      System.out.println(fullName.length());
+
+//     for(int i=0;i<fullName.length();i++){
+//         System.out.println(fullName.charAt(i));
+//     }
+//     }
+// }
+
+
+
+public class java21{
+    public static void main(String args[]){
+      String name1="Tony";
+      String name2="Tony2";
+
+      if(name1.compareTo(name2)==0){
+        System.out.println("Strings are equal");
+      }else{
+        System.out.println("String  are not equal");
+      }
+    }
+}

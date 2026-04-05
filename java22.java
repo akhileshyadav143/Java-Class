18830273930be7f224cd4a945bb388a3ae8c3329
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-04-05 14:38:54 +0530

    java

---
+++ java22.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class java22 {
+    public  static void main(String args[]){
+        String sentence = "My name is tony";
+        String name = sentence.substring(11, sentence.length());
+        System.out.println(name);;   
+    }
+}

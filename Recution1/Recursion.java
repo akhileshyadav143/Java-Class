27695b6f2ed6335351e6f2afcc381864228ecf05
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-05-02 23:12:12 +0530

    java

--- Recution1/Recursion.java
+++ Recution1/Recursion.java
@@ -1,34 +1,34 @@
-// package Recution1;
-// import java.util.Arrays;
+package Recution1;
+import java.util.Arrays;
 
 
-// public class Recursion {
+public class Recursion {
 
-//     static int ways(String str, int i, int[] dp){
-//         if(i == str.length()) return 0;
+    static int ways(String str, int i, int[] dp){
+        if(i == str.length()) return 0;
 
-//         if(dp[i] != -1) return dp[i];
+        if(dp[i] != -1) return dp[i];
 
-//         char ch = str.charAt(i);
+        char ch = str.charAt(i);
 
-//         int count = 0;
+        int count = 0;
 
-//         if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
-//             count = 1;
-//         }
+        if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
+            count = 1;
+        }
 
-//         dp[i] = count + ways(str, i+1, dp);
+        dp[i] = count + ways(str, i+1, dp);
 
-//         return dp[i];
-//     }
+        return dp[i];
+    }
 
-//     public static void main(String[] args){
-//         String str = "education";
-//         int dp[] = new int[str.length()];
-//         Arrays.fill(dp, -1);
+    public static void main(String[] args){
+        String str = "education";
+        int dp[] = new int[str.length()];
+        Arrays.fill(dp, -1);
 
-//         int i = 0;
+        int i = 0;
 
-//         System.out.println(ways(str, i, dp));
-//     }
-// }
+        System.out.println(ways(str, i, dp));
+    }
+}
---
+++ Recution1/tempCodeRunnerFile.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class Recution1 {
+    
+    static int ways(int n, int[] dp){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        dp[n] = ways(n-1, dp) + ways(n-2, dp);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        System.out.println(ways(n, dp));
+    }
+}
+

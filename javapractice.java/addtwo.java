56fc373e14324cefbe38c49c201ec281eeb691da
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-05-03 23:50:42 +0530

    java

---
+++ javapractice.java/addtwo.java
@@ -0,0 +1,28 @@
+// package javapractice.java;
+
+// public class addtwo {
+//     public static void main(String[] args)
+//     {
+//         int a=2;
+//         int b=3;
+//         int c=a+b;
+//         System.out.println(c);
+//     }
+// }
+
+
+
+//swap
+ package javapractice.java;
+ public  class swap{
+    public static void main(String[] args) {
+        
+    
+    int a=9, b=3;
+    System.out .println("before swaping: a="+a+",b="+b);
+    int temp=a;
+    a=b;
+    b=temp;
+    System.out .println("after swaping: a="+a+",b="+b);
+    }
+ }
---
+++ javapractice.java/tempCodeRunnerFile.java
@@ -0,0 +1,13 @@
+ package javapractice.java;
+ public  class swap{
+    public static void main(String[] args) {
+        
+    
+    int a=9, b=3;
+    System.out .println("before swaping: a="+a+",b="+b);
+    int temp=a;
+    a=b;
+    b=temp;
+    System.out .println("after swaping: a="+a+",b="+b);
+    }
+ }

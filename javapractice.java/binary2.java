80f600d260fe6fbeb1b3108eaffd604d6661fae5
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-05-07 22:56:59 +0530

    java

---
+++ javapractice.java/binary2.java
@@ -0,0 +1,21 @@
+  import java.io.*;
+public class binary2 {
+
+
+    static int countSetBits(int n)
+    {
+        int count = 0;
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+
+    public static void main(String args[])
+    {
+        int i = 9;
+        System.out.println(countSetBits(i));
+    }
+}
+

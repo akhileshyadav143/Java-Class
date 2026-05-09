9bbe7e355b1d12f28490791e15bb0711c67d9313
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-05-09 19:41:01 +0530

    java

---
+++ javabasic/javap1.java
@@ -0,0 +1,72 @@
+
+
+// public class javap1 {
+ 
+//     public static void main(String[] args) {
+//         int x=500;
+//         System.out.println(x);
+
+//     x=x+5;
+//     System.out.println(x);
+//     x=x-10;
+//     System.out.println(x);
+//     }
+// }
+
+
+
+// public class javap1 {
+ 
+//     public static void main(String[] args) {
+//         int x=5;
+//         System.out.println(x);
+//         x=7;
+//         System.out.println(x);
+//         x=x+5;
+//         System.out.println(x);
+//         x=x-20;
+//         System.out.println(x);
+//     }
+// }
+
+//  public class javap1 {
+ 
+//      public static void main(String[] args) {
+
+//         int x=5;
+//         int y=10;
+//         System.out.println(x-y);
+//          System.out.println(x+y);
+//           System.out.println(x*y);
+//            System.out.println(x/y);
+         
+//      }
+//      }
+
+
+//  public class javap1 {
+ 
+//      public static void main(String[] args) {
+//         int x=4,y=3,z=9;
+//         System.out.println("the va.ue of x is"+x);
+//         System.out.println("the value of y is"+y);
+//         z=10;
+//         System.out.println(z);
+//         System.out.println(x+y+z);
+//         System.out.println("hello"+x+y+z);
+//      }
+//     }
+
+    // incriment decriment
+     public class javap1 {
+ 
+     public static void main(String[] args) {
+        int x=40;
+        System.out.println(x);
+        x++;
+       
+        
+       System.out.println(x);
+
+     }
+    }

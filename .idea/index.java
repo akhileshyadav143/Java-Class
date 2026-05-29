c7a0a3f18a506224c0cb218503fde3fbbd5344c7
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-05-29 22:40:35 +0530

    java

---
+++ .idea/index.java
@@ -0,0 +1,32 @@
+// import java.util.Scanner;
+
+// public class index {
+//   public static void main(String[] arg){
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("Enter your name:");
+//     String n=sc.nextLine();
+//    System.out.println("your name is: "+n);
+//   }
+    
+// }
+// public class index{
+//     public static void main(String[] args) {
+//             int n=100;
+//         for( int i=1;i<=n;i=i+2)
+//             System.out.println(i);
+//          }
+// }
+
+
+
+
+// public class index{
+//     public static void main(String[] args) {
+//         int n=5, fact=1;
+//         for(int i=1;i<=n;i++)
+//         fact=fact*i;
+
+//      System.out.println(fact);
+     
+//     }
+// }

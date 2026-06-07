24afade2f1c97bf8cf514a99cec623e9555030b0
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-06-08 00:02:28 +0530

    sort

---
+++ sorting.java/Bubblesort.java
@@ -0,0 +1,29 @@
+package sorting.java;
+
+public class Bubblesort {
+    public static void bubblesort(int arr[]) {
+        int n = arr.length;
+
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 6, 4, 7};
+
+        bubblesort(arr);
+
+        System.out.println("printing sorted array");
+
+        for(int value : arr) {
+            System.out.println(value);
+        }
+    }
+}

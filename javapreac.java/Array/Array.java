1eb7e1d4c7d856e452b04cdea44f3e0e7998c6d5
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-09-25 22:40:14 +0530

    array

---
+++ javapreac.java/Array/Array.java
@@ -0,0 +1,11 @@
+
+
+public class Array {
+    public static void main(String[] args) {
+        // int [] x= {6,7,19,46,171,9};
+        //   System.out.println(x[0]);
+        //   x[4]=45;
+        //   System.out.println(x[4]);
+        int arr[]=new int[4];
+    }
+}
